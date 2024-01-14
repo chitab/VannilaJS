5537836efb2caa6e9d29d6e7720acb42587946bb
Chitab Kumar <35214320+chitab@users.noreply.github.com>
2024-01-14 14:59:54 +0530

    added search box

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputValue = document.querySelector('#items');
 const btn = document.querySelector('#submit');
 const ulList = document.querySelector('.ul-list');
+const searchInput = document.querySelector('#search-input');
 btn.addEventListener('click', (e)=> {
     e.preventDefault();
     if(inputValue.value.length > 0){
@@ -22,6 +23,19 @@ ulList.addEventListener('click', (e) => {
     if(e.target.classList.contains('remove-btn')){
         e.target.parentElement.remove();
     }
+});
+searchInput.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    const text = e.target.value.toLowerCase();
+    const li = ulList.getElementsByTagName('li');
+    Array.from(li).forEach((item) => {
+        const itemName = item.firstChild.textContent;
+        if(itemName.toLocaleLowerCase().indexOf(text) != -1){
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none';
+        }
+    })
 })
  
 
